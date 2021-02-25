0e8eb23c44823d873c71b8696f8338eb292f0867
linxianwo <linxianwo@163.com>
2021-02-25 10:19:26 +0800

    Create internalParam-model.ts

---
+++ share/main-proc/internalParam/internalParam-model.ts
@@ -0,0 +1,10 @@
+// this class to log down info , during app start up till print the log
+class InternalContent {
+    public msn = 0;
+    public currentDate = Date.now();
+    public signonTime = '';
+
+    constructor() {}
+}
+
+export { InternalContent };
